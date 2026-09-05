70192709e2897a03eef463dae585473db7c18c19
Tom Hunze <dev@thunze.de>
2026-09-05 09:04:24 +0200

    pangomm_{1_4,2_48}: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5764,8 +5764,7 @@ with pkgs;
   # Not moved to aliases while we decide if we should split the package again.
   atk = at-spi2-core;
 
-  pangomm_1_4 = callPackage ../development/libraries/pangomm { };
-  pangomm_2_48 = callPackage ../development/libraries/pangomm/2.48.nix { };
+  pangomm_1_4 = callPackage ../by-name/pa/pangomm_2_48/1.4.nix { };
 
   gtk2-x11 = gtk2.override {
     cairo = cairo.override { x11Support = true; };

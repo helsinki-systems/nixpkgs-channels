d3c747a9ba3b1773824b052ed1701f316d8c8052
Tom Hunze <dev@thunze.de>
2026-09-05 09:00:05 +0200

    pangomm: rename to pangomm_1_4
    Having the older ABI version as the default seems odd and the different
    pangomm packages appear to be fundamentally incompatible, so drop the
    unsuffixed version similar to webkitgtk and as discussed in [1].
    
    [1] https://github.com/NixOS/nixpkgs/pull/543345#issuecomment-5199018730

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5764,8 +5764,7 @@ with pkgs;
   # Not moved to aliases while we decide if we should split the package again.
   atk = at-spi2-core;
 
-  pangomm = callPackage ../development/libraries/pangomm { };
-
+  pangomm_1_4 = callPackage ../development/libraries/pangomm { };
   pangomm_2_48 = callPackage ../development/libraries/pangomm/2.48.nix { };
 
   gtk2-x11 = gtk2.override {

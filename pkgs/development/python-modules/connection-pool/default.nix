9fb11acbc06756831beb8ddaf500dead029d324e
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:14 +0700

    python3Packages.connection-pool: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/connection-pool/default.nix
+++ pkgs/development/python-modules/connection-pool/default.nix
@@ -2,13 +2,14 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   isPy3k,
 }:
 
 buildPythonPackage rec {
   pname = "connection-pool";
   version = "0.0.3";
-  format = "setuptools";
+  pyproject = true;
   disabled = !isPy3k;
 
   src = fetchPypi {
@@ -16,6 +17,8 @@ buildPythonPackage rec {
     inherit version;
     sha256 = "bf429e7aef65921c69b4ed48f3d48d3eac1383b05d2df91884705842d974d0dc";
   };
+
+  build-system = [ setuptools ];
 
   doCheck = false; # no tests
   pythonImportsCheck = [ "connection_pool" ];

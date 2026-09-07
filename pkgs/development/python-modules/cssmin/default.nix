e6c4442b3981c3d2a031217dbb97c1250a307322
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:18 +0700

    python3Packages.cssmin: modernize

--- pkgs/development/python-modules/cssmin/default.nix
+++ pkgs/development/python-modules/cssmin/default.nix
@@ -5,14 +5,16 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "cssmin";
   version = "0.2.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "1dk723nfm2yf8cp4pj785giqlwv42l0kj8rk40kczvq1hk6g04p0";
+    inherit (finalAttrs) pname version;
+    hash = "sha256-4BLwzIQB788mIDMjOQEVZHOK4yvoyEsuQ86L6uwQZ7Y=";
   };
 
   build-system = [ setuptools ];
@@ -20,10 +22,12 @@ buildPythonPackage rec {
   # no tests
   doCheck = false;
 
+  pythonImportsCheck = [ "cssmin" ];
+
   meta = {
     description = "Python port of the YUI CSS compression algorithm";
     mainProgram = "cssmin";
     homepage = "https://github.com/zacharyvoase/cssmin";
     license = lib.licenses.bsd3;
   };
-}
+})

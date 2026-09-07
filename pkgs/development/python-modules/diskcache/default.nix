3106003bfb4adcf4d1ad51e9811d42af58b7296c
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:23 +0700

    python3Packages.diskcache: modernize

--- pkgs/development/python-modules/diskcache/default.nix
+++ pkgs/development/python-modules/diskcache/default.nix
@@ -10,15 +10,17 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "diskcache";
   version = "5.6.3";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "grantjenks";
     repo = "python-diskcache";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-1cDpdf+rLaG14TDd1wEHAiYXb69NFTFeOHD1Ib1oOVY=";
   };
 
@@ -51,4 +53,4 @@ buildPythonPackage rec {
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
-}
+})

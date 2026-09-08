8ada5d6c96198fabf9257ca2b506572c410dc2de
airone01 <airone01@proton.me>
2026-09-08 16:03:35 +0200

    docker-sbom: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8375,7 +8375,6 @@ with pkgs;
 
   docker-gc = callPackage ../applications/virtualization/docker/gc.nix { };
   docker-buildx = callPackage ../applications/virtualization/docker/buildx.nix { };
-  docker-sbom = callPackage ../applications/virtualization/docker/sbom.nix { };
 
   drawpile-server-headless = drawpile.override {
     buildClient = false;

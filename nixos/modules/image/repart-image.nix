0dc68a4c775abcc52fc347986a585106fc644297
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-09-08 23:08:36 +0200

    nixos/image/repart-image: fix Format=empty
    (cherry picked from commit e72abfc4a0d77661f668efbaf1493f21617982fb)

--- nixos/modules/image/repart-image.nix
+++ nixos/modules/image/repart-image.nix
@@ -110,6 +110,7 @@ let
     "btrfs" = [ btrfs-progs ];
     "xfs" = [ xfsprogs ];
     "swap" = [ util-linux ];
+    "empty" = [ ];
   };
 
   fileSystemTools = builtins.concatMap (f: fileSystemToolMapping."${f}") fileSystems;

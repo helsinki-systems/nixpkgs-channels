b460364ebbed9d6170eca9efa71a8441cd4524bd
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-09 15:36:08 +0000

    python3Packages.publicsuffixlist: 1.0.2.20260906 -> 1.0.2.20260909

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "publicsuffixlist";
-  version = "1.0.2.20260906";
+  version = "1.0.2.20260909";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-pJH8PTa8BU+zelyUBmD9NRaHiD6Uh+M2ULKghFoJAdo=";
+    hash = "sha256-/yT1gdIY0jLbFS6CZDzdbM66zJLuH6girv+voH3MN90=";
   };
 
   postPatch = ''

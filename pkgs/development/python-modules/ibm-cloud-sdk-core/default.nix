b2c88c5c57103d89b9dfb22586abfcedec275a69
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-10 10:57:10 +0200

    python3Packages.ibm-cloud-sdk-core: migrate to finalAttrs

--- pkgs/development/python-modules/ibm-cloud-sdk-core/default.nix
+++ pkgs/development/python-modules/ibm-cloud-sdk-core/default.nix
@@ -10,7 +10,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "ibm-cloud-sdk-core";
   version = "3.26.1";
   pyproject = true;
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "IBM";
     repo = "python-sdk-core";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-8gZrjPlvYY3KCDVDqjQUdGpgGo/XrxHS0Z7PMmHdh5g=";
   };
 
@@ -62,8 +62,8 @@ buildPythonPackage rec {
   meta = {
     description = "Client library for the IBM Cloud services";
     homepage = "https://github.com/IBM/python-sdk-core";
-    changelog = "https://github.com/IBM/python-sdk-core/blob/${src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/IBM/python-sdk-core/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
-}
+})

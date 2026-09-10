4aaf9f6f4b9621d0dc0d104d8f16abc209f54f6e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-09-10 07:44:35 +0200

    tgrep: 1.0.4 -> 1.0.5
    Diff: https://github.com/microsoft/tgrep/compare/v1.0.4...v1.0.5
    
    Changelog: https://github.com/microsoft/tgrep/releases/tag/v1.0.5

--- pkgs/by-name/tg/tgrep/package.nix
+++ pkgs/by-name/tg/tgrep/package.nix
@@ -10,7 +10,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "tgrep";
-  version = "1.0.4";
+  version = "1.0.5";
   __structuredAttrs = true;
   __darwinAllowLocalNetworking = true;
 
@@ -18,10 +18,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "microsoft";
     repo = "tgrep";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-t+gtDMpoxuRN2K6xeztNcOJMuc4eGnF8H3sacN21UF4=";
+    hash = "sha256-Iru2pnpcassf3oKTxfBBzWQ/MbVI6OkUbaQinbNcpXo=";
   };
 
-  cargoHash = "sha256-Vtqx76DHnsP6gexjTPj0hfCGHnS5yQ5xM+7RbgwrzAA=";
+  cargoHash = "sha256-xwZ14In14bvLLs34bUwqCt87Zv1jiXIJCtrqOnwbt8w=";
 
   doInstallCheck = true;
   nativeInstallCheckInputs = [ versionCheckHook ];

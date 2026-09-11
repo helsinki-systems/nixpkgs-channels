7e2a57ae3ef27209cf6611464fba97e6e0badec1
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-11 15:15:43 +0200

    python3Packages.llama-index-readers-s3: migrate to finalAttrs

--- pkgs/development/python-modules/llama-index-readers-s3/default.nix
+++ pkgs/development/python-modules/llama-index-readers-s3/default.nix
@@ -9,14 +9,14 @@
   s3fs,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "llama-index-readers-s3";
   version = "0.7.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "llama_index_readers_s3";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-CC3cO+/ns2USy2fif9gXzHYqoFRNa8RxH29gXFhktO8=";
   };
 
@@ -40,4 +40,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})

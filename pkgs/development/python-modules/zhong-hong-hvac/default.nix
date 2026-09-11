e81ce467062829fdf4547bafb0b361d86509605a
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-11 03:45:00 +0000

    python3Packages.zhong-hong-hvac: 1.0.19 -> 1.0.21

--- pkgs/development/python-modules/zhong-hong-hvac/default.nix
+++ pkgs/development/python-modules/zhong-hong-hvac/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "zhong-hong-hvac";
-  version = "1.0.19";
+  version = "1.0.21";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "crhan";
     repo = "ZhongHongHVAC";
     tag = "v${version}";
-    hash = "sha256-MRtjQ2l5w/iDungzfNNWPGC4lTkmz+aWiny4kqG/Y3A=";
+    hash = "sha256-epU8ll8CCgc6ICxKr0duebX8wrtnXOPvADftk8EJsdw=";
   };
 
   build-system = [ poetry-core ];

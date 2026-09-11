ae77bf3fc779b1779bf5ffc749f2f8048b4bc853
R. Ryantm <ryantm-bot@ryantm.com>
2026-09-11 10:42:44 +0000

    python3Packages.llama-index-vector-stores-google: 0.5.0 -> 0.6.0

--- pkgs/development/python-modules/llama-index-vector-stores-google/default.nix
+++ pkgs/development/python-modules/llama-index-vector-stores-google/default.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "llama-index-vector-stores-google";
-  version = "0.5.0";
+  version = "0.6.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "llama_index_vector_stores_google";
     inherit version;
-    hash = "sha256-lf1Wr8l6azfxrokcGilR+IriU465LmFXDiqfHrCdrO0=";
+    hash = "sha256-bGxsNAE6tC0bOLfjzn3kY1Se17yh6c7jjk/sgyMPdck=";
   };
 
   pythonRelaxDeps = [ "google-generativeai" ];

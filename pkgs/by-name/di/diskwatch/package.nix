738e2aed9f287132de043007107921b12a27834e
Fabian Affolter <mail@fabian-affolter.ch>
2026-09-12 09:15:11 +0200

    diskwatch: 0.5.2 -> 0.5.4
    Diff: https://github.com/matthart1983/diskwatch/compare/v0.5.2...v0.5.4
    
    Changelog: https://github.com/matthart1983/diskwatch/releases/tag/v0.5.4

--- pkgs/by-name/di/diskwatch/package.nix
+++ pkgs/by-name/di/diskwatch/package.nix
@@ -11,18 +11,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "diskwatch";
-  version = "0.5.2";
+  version = "0.5.4";
 
   src = fetchFromGitHub {
     owner = "matthart1983";
     repo = "diskwatch";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-aebepClhjC5Gnw9Ct+z04XX0bcDRytk2Z88BeA0lE8Y=";
+    hash = "sha256-gDgF8PFyiMVk/XEiLPt8kPc50tjqiUm1FvCZVKX5f9M=";
   };
 
   __structuredAttrs = true;
 
-  cargoHash = "sha256-+TY+EY3VfMLONYYVH+HdoKdUKXPSer4UvpVgNH59hHI=";
+  cargoHash = "sha256-4gTWfdJswuf7vISA1aIF/nVuzIrIIoI0GSnRCCYC/pA=";
 
   nativeCheckInputs = [ versionCheckHook ];
 
